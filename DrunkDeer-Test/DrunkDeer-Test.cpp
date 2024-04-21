5a7b28680f3877b22175f8102f23827c763f75cd
wolf109909 <84360921+wolf109909@users.noreply.github.com>
2024-04-21 17:58:38 +0800

    add deadzones, improve config

--- DrunkDeer-Test/DrunkDeer-Test.cpp
+++ DrunkDeer-Test/DrunkDeer-Test.cpp
@@ -6,17 +6,17 @@
 #include <chrono>
 #include <cstdio>
 #include <cstdlib>
+#include <fstream>
 #include <hidapi.h>
 #include <iostream>
-#include <fstream>
 #include <limits>
+#include <map>
+#include <nlohmann/json.hpp>
 #include <spdlog/spdlog.h>
 #include <thread>
 #include <unordered_map>
-#include <map>
 #include <vector>
 #include <windows.h>
-#include <nlohmann/json.hpp>
 
 #pragma comment(lib, "setupapi.lib")
 
@@ -31,7 +31,8 @@ XINPUT_STATE g_xinput_state;
 DWORD g_xinput_packet_count = 0;
 std::atomic_bool should_send = false;
 int g_current_keyboard_identifier = 0;
-
+int g_deadzone_max = 36;
+int g_deadzone_min = 2;
 // keyboard packet -> find in keycode2action map -> call action func
 
 typedef void (*pad_action_func)(int keycode, double percent, bool inverse);
@@ -50,27 +51,26 @@ struct pad_action {
 
 std::map<uint8_t, pad_action> g_key_action_map;
 
-
-
 std::vector<std::string> keyboard_layout_a75 = {
-    "ESC",  "",  "",        "F1",     "F2",     "F3",      "F4",      "F5",
-    "F6",     "F7",      "F8",     "F9",     "F10",     "F11",     "F12",
-    "KP7",    "KP8",     "KP9",    "u1",     "u2",      "u3",      "u4",
-    "SWUNG",  "1",       "2",      "3",      "4",       "5",       "6",
-    "7",      "8",       "9",      "0",      "MINUS",   "PLUS",    "BACK",
-    "KP4",    "KP5",     "KP6",    "u5",     "u6",      "u7",      "u8",
-    "TAB",    "Q",       "W",      "E",      "R",       "T",       "Y",
-    "U",      "I",       "O",      "P",      "BRKTS_L", "BRKTS_R", "SLASH_K29",
-    "KP1",    "KP2",     "KP3",    "u9",     "u10",     "u11",     "u12",
-    "CAPS",   "A",       "S",      "D",      "F",       "G",       "H",
-    "J",      "K",       "L",      "COLON",  "QOTATN",  "u13",     "RETURN",
-    "u14",    "KP0",     "KP_DEL", "u15",    "u16",     "u17",     "u18",
-    "SHF_L",  "EUR_K45", "Z",      "X",      "C",       "V",       "B",
-    "N",      "M",       "COMMA",  "PERIOD", "VIRGUE",  "u19",     "SHF_R",
-    "ARR_UP", "u20",     "NUMS",   "u21",    "u22",     "u23",     "u24",
-    "CTRL_L", "WIN_L",   "ALT_L",  "u25",    "u26",     "u27",     "SPACE",
-    "u28",    "u29",     "u30",    "ALT_R",  "FN1",     "APP", "",    "ARR_L",
-    "ARR_DW", "ARR_R",   "CTRL_R", "u31",    "u32",     "u33",     "u34"};
+    "ESC",       "",       "",        "F1",     "F2",     "F3",      "F4",
+    "F5",        "F6",     "F7",      "F8",     "F9",     "F10",     "F11",
+    "F12",       "KP7",    "KP8",     "KP9",    "u1",     "u2",      "u3",
+    "u4",        "SWUNG",  "1",       "2",      "3",      "4",       "5",
+    "6",         "7",      "8",       "9",      "0",      "MINUS",   "PLUS",
+    "BACK",      "KP4",    "KP5",     "KP6",    "u5",     "u6",      "u7",
+    "u8",        "TAB",    "Q",       "W",      "E",      "R",       "T",
+    "Y",         "U",      "I",       "O",      "P",      "BRKTS_L", "BRKTS_R",
+    "SLASH_K29", "KP1",    "KP2",     "KP3",    "u9",     "u10",     "u11",
+    "u12",       "CAPS",   "A",       "S",      "D",      "F",       "G",
+    "H",         "J",      "K",       "L",      "COLON",  "QOTATN",  "u13",
+    "RETURN",    "u14",    "KP0",     "KP_DEL", "u15",    "u16",     "u17",
+    "u18",       "SHF_L",  "EUR_K45", "Z",      "X",      "C",       "V",
+    "B",         "N",      "M",       "COMMA",  "PERIOD", "VIRGUE",  "u19",
+    "SHF_R",     "ARR_UP", "u20",     "NUMS",   "u21",    "u22",     "u23",
+    "u24",       "CTRL_L", "WIN_L",   "ALT_L",  "u25",    "u26",     "u27",
+    "SPACE",     "u28",    "u29",     "u30",    "ALT_R",  "FN1",     "APP",
+    "",          "ARR_L",  "ARR_DW",  "ARR_R",  "CTRL_R", "u31",     "u32",
+    "u33",       "u34"};
 
 void pad_action_leftstick_x_positive(int keycode, double percent,
                                      bool inverse) {
@@ -149,29 +149,27 @@ void load_function_map() {
       std::make_pair("RTrigger", pad_action_right_trigger_analog));
 }
 
-
- hid_device *open_target_device(struct hid_device_info *cur_dev) {
-   for (; cur_dev; cur_dev = cur_dev->next) {
-     if (cur_dev->vendor_id == 0x352D &&
-         (cur_dev->product_id == 0x2383 || cur_dev->product_id == 0x2382 ||
-          cur_dev->product_id == 0x2384) &&
-         cur_dev->usage == 0x0) {
-       printf("Device found: %s\n", cur_dev->path);
-       return hid_open_path(cur_dev->path);
-     }
-   }
- }
-
+hid_device *open_target_device(struct hid_device_info *cur_dev) {
+  for (; cur_dev; cur_dev = cur_dev->next) {
+    if (cur_dev->vendor_id == 0x352D &&
+        (cur_dev->product_id == 0x2383 || cur_dev->product_id == 0x2382 ||
+         cur_dev->product_id == 0x2384) &&
+        cur_dev->usage == 0x0) {
+      printf("Device found: %s\n", cur_dev->path);
+      return hid_open_path(cur_dev->path);
+    }
+  }
+}
 
 void key_height_handler(uint8_t keycode, double travel) {
 
-  auto& action = g_key_action_map[keycode];
+  auto &action = g_key_action_map[keycode];
   if (action.f != nullptr) {
     action.f(keycode, travel, action.inverse);
-    
+
   } else {
-    //spdlog::error("Action function pointer for key:{} is valid!",
-      //            keyboard_layout_a75[keycode]);
+    // spdlog::error("Action function pointer for key:{} is valid!",
+    //             keyboard_layout_a75[keycode]);
   }
 }
 
@@ -185,11 +183,11 @@ void keyboard_identity_handler(unsigned char byte5, unsigned char byte6,
   } else if ((byte5 == 11) && (byte6 == 4) && (byte7 == 2)) {
     int current_iso = 751; // Assume UK as default
     if (current_iso == 751) {
-       g_current_keyboard_identifier = 751;
+      g_current_keyboard_identifier = 751;
     } else if (current_iso == 752) {
-       g_current_keyboard_identifier = 752;
+      g_current_keyboard_identifier = 752;
     } else if (current_iso == 753) {
-       g_current_keyboard_identifier = 753;
+      g_current_keyboard_identifier = 753;
     }
   } else if (((byte5 == 11) && (byte6 == 2) && (byte7 == 1)) ||
              ((byte5 == 15) && (byte6 == 1) && (byte7 == 1))) {
@@ -276,20 +274,16 @@ void receive_packet_controller(PVIGEM_CLIENT client, PVIGEM_TARGET pad,
   unsigned char byte6 = buffer[6];
   unsigned char byte7 = buffer[7];
 
-
-    if(command == 0xa0){
+  if (command == 0xa0) {
     if (byte2 == 0x02 && byte3 == 0x00) {
-        // keyboard respond identify command
-       keyboard_identity_handler(byte5, byte6, byte7);
-       
-       spdlog::info("Connected to keyboard: {}",
-           get_keyboard_name_from_id(g_current_keyboard_identifier));
-       return;
-          
-    }
-    }
+      // keyboard respond identify command
+      keyboard_identity_handler(byte5, byte6, byte7);
 
-  
+      spdlog::info("Connected to keyboard: {}",
+                   get_keyboard_name_from_id(g_current_keyboard_identifier));
+      return;
+    }
+  }
 
   if (command != 0xb7) {
     // we only need key height message here
@@ -314,17 +308,20 @@ void receive_packet_controller(PVIGEM_CLIENT client, PVIGEM_TARGET pad,
 
     int keynum = base_position + i;
     unsigned char new_value = buffer[i + 4];
-    if (new_value < 2) {
+    if (new_value < g_deadzone_min) {
       new_value = 0;
     }
-    double p = new_value / 40.0;
-    if (int(new_value) >= 10) {
-        std::cout << "key: " << keynum << "(" << keyboard_layout_a75[keynum] << ")"
-                << " , height: " << int(new_value) << std::endl;
+    if (new_value > g_deadzone_max) {
+      new_value = 40;
     }
-        
+    double p = new_value / 40.0;
+    /*if (int(new_value) >= 10) {
+      std::cout << "key: " << keynum << "(" << keyboard_layout_a75[keynum]
+                << ")"
+                << " , height: " << int(new_value) << std::endl;
+    }*/
+
     key_height_handler(keynum, p);
-    
   }
   if (byte4 != 0x00 && byte4 != 0x01) {
 
@@ -421,40 +418,47 @@ uint8_t keyname_to_index(std::vector<std::string> v, std::string k) {
   }
 }
 
-void register_key_action(std::string keyname, std::string padname,bool inverse) {
+void register_key_action(std::string keyname, std::string padname,
+                         bool inverse) {
   auto operation = pad_name_lookup_table[padname];
   uint8_t keycode = keyname_to_index(keyboard_layout_a75, keyname);
 
   if (keycode == -1) {
-    spdlog::error("failed to find keycode for {}, ignoring.",keyname);
+    spdlog::error("failed to find keycode for {}, ignoring.", keyname);
     return;
   }
-    
-  
+
   pad_action act;
   act.f = operation;
   act.inverse = inverse;
 
-
   g_key_action_map.insert(std::make_pair(keycode, act));
-  spdlog::info("registered {}({}) to {}", keyboard_layout_a75[keycode],keycode, padname);
+  spdlog::info("registered {}({}) to {}", keyboard_layout_a75[keycode], keycode,
+               padname);
 }
 
-void initialize_action_map(nlohmann::json config) 
-{ 
-    for (const auto &item : config) {
-    std::string key = item["Key"];
-    std::string action = item["Action"];
-    register_key_action(key, action, false);
+void initialize_action_map(nlohmann::json config) {
+  try {
+      g_deadzone_min = config.at("DeadZoneMin");
+      g_deadzone_max = config.at("DeadZoneMax");
+      spdlog::info("Active deadzones: 0-{}, {}-40", g_deadzone_min, g_deadzone_max);
+      for (const auto &item : config["ActionMaps"]) {
+        std::string key = item["Key"];
+        std::string action = item["Action"];
+        bool invert = item.at("Invert");
+        register_key_action(key, action, invert);
+      }
+  } catch (const nlohmann::json::type_error &e) {
+    spdlog::error("Error while loading configuration file! Details: {}",
+                  e.what());
+                
   }
-
-
-    /*register_key_action("ARR_UP", "RStickY+",false);
-    register_key_action("ARR_DW", "RStickY-", false);
-    register_key_action("ARR_R", "RStickX+", false);
-    register_key_action("ARR_L", "RStickX-", false);
-    register_key_action("W", "RTrigger", false);
-    register_key_action("S", "LTrigger", false);*/
+  /*register_key_action("ARR_UP", "RStickY+",false);
+  register_key_action("ARR_DW", "RStickY-", false);
+  register_key_action("ARR_R", "RStickX+", false);
+  register_key_action("ARR_L", "RStickX-", false);
+  register_key_action("W", "RTrigger", false);
+  register_key_action("S", "LTrigger", false);*/
 }
 
 int main(int argc, char *argv[]) {
@@ -493,7 +497,8 @@ int main(int argc, char *argv[]) {
   const auto retval = vigem_connect(client);
 
   if (!VIGEM_SUCCESS(retval)) {
-    spdlog::error("ViGEm Bus connection failed with error code: {}", uint64_t(retval));
+    spdlog::error("ViGEm Bus connection failed with error code: {}",
+                  uint64_t(retval));
     return -1;
   }
   // new handle for virtual pad
@@ -511,20 +516,16 @@ int main(int argc, char *argv[]) {
 
   // ===============================================
   // Configuration file init
-  
-  try 
-  {
+
+  try {
     std::ifstream f("config.json");
-    load_function_map(); 
+    load_function_map();
     nlohmann::json config = nlohmann::json::parse(f);
     initialize_action_map(config);
-  } 
-  catch (const std::exception &e) {
+  } catch (const std::exception &e) {
     spdlog::error("Error while reading config file: \n{}", e.what());
   }
-  
 
-  
   // End of Configuration file init
   // ===============================================
 
@@ -532,7 +533,7 @@ int main(int argc, char *argv[]) {
   if (g_current_keyboard_identifier == 0) {
     sendpkt_request_identity();
   }
-      
+
   // initial packet used to trigger the loop
   sendpkt_request_keys();
 
@@ -557,26 +558,32 @@ int main(int argc, char *argv[]) {
       printf("Unable to read(): %ls\n", hid_error(g_hid_device_handle));
       break;
     } else if (res > 0) {
-     receive_packet_controller(client, pad, hid_readbuffer);
-      /*
-      conout.setpos(0, 10);
-      std::cout << "sThumbLX:  " << g_xinput_state.Gamepad.sThumbLX
-                << "        ";
-      conout.setpos(0, 11);
-      std::cout << "sThumbLY:  " << g_xinput_state.Gamepad.sThumbLY
-                << "        ";
-      conout.setpos(0, 12);
-      std::cout << "sThumbRX:  " << g_xinput_state.Gamepad.sThumbRX
-                << "        ";
-      conout.setpos(0, 13);
-      std::cout << "sThumbRY:  " << g_xinput_state.Gamepad.sThumbRY
-                << "        ";
-      conout.setpos(0, 14);
-      std::cout << "bLeftTrigger:  " << int(g_xinput_state.Gamepad.bLeftTrigger)
-                << "        ";
-      conout.setpos(0, 15);
-      std::cout << "bRightTrigger: "
-                << int(g_xinput_state.Gamepad.bRightTrigger) << "        ";*/
+
+      if (g_current_keyboard_identifier != 0) {
+        conout.setpos(0, 20);
+        std::cout << "sThumbLX:  " << g_xinput_state.Gamepad.sThumbLX
+                  << "        ";
+        conout.setpos(0, 21);
+        std::cout << "sThumbLY:  " << g_xinput_state.Gamepad.sThumbLY
+                  << "        ";
+        conout.setpos(0, 22);
+        std::cout << "sThumbRX:  " << g_xinput_state.Gamepad.sThumbRX
+                  << "        ";
+        conout.setpos(0, 23);
+        std::cout << "sThumbRY:  " << g_xinput_state.Gamepad.sThumbRY
+                  << "        ";
+        conout.setpos(0, 24);
+        std::cout << "bLeftTrigger:  "
+                  << int(g_xinput_state.Gamepad.bLeftTrigger) << "        ";
+        conout.setpos(0, 25);
+        std::cout << "bRightTrigger: "
+                  << int(g_xinput_state.Gamepad.bRightTrigger) << "        ";
+      }
+      
+
+      receive_packet_controller(client, pad, hid_readbuffer);
+      
+      
     }
   }
 
